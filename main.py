6e2265668199a468623e61725b8e035d4f363301
Buzato <joaobuzato@gmail.com>
2022-05-02 18:40:54 -0300

    exercises now fill spreadsheet with info

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import datetime
 import requests
 
 NUTRITIONIX_APP_ID = os.environ.get('NUTRITIONIX_APP_ID')
@@ -21,3 +21,23 @@ json_natural_exercises = {
 
 response = requests.post(nutritionix_natual_url, json=json_natural_exercises, headers=headers)
 print(response.json())
+
+today = datetime.now()
+
+sheety_url = "https://api.sheety.co/de21026eb64e4f9375c8bb7d7ab06f1a/day38Yu/workouts"
+exercises = response.json()['exercises']
+for exercise in exercises:
+
+    params = {
+        "workout": {
+            "date": datetime.strftime(today, "%d/%m/%Y"),
+            "time": datetime.strftime(today, "%H:%M:%S"),
+            "exercise": exercise['name'],
+            "duration": exercise['duration_min'],
+            "calories": exercise['nf_calories']
+        }
+    }
+    sheety_headers = {
+        "Authorization": os.environ.get('SHEETY_AUTH')
+    }
+    requests.post(sheety_url, json=params, headers=sheety_headers)
